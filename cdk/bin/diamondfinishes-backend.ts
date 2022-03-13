9ca1332bdb66f43e07be44d266526e294396376f
QuintinChris <quintinchrisdev@gmail.com>
2022-03-13 11:54:40 -0400

    initial template generation with cdk

--- cdk/bin/diamondfinishes-backend.ts
+++ cdk/bin/diamondfinishes-backend.ts
@@ -1,21 +1,30 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { DiamondfinishesBackendStack } from '../lib/diamondfinishes-backend-stack';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as aws_apigateway from "aws-cdk-lib/aws-apigateway";
+import { join } from "path";
+export class DiamondfinishesBackendStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const sendEmailHandler = new lambda.Function(this, "sendEmailHandler", {
+      code: lambda.Code.fromAsset(join(__dirname, "../../handlers/sendEmail")),
+      runtime: lambda.Runtime.NODEJS_12_X,
+      functionName: "sendEmailHandler",
+      description: "handles sending emails to user",
+      handler: "../handlers/sample.handler.ts",
+    });
+
+    new aws_apigateway.LambdaRestApi(this, "DiamondFinishesAPI", {
+      description: "API Gateway for Diamond Finishes",
+      restApiName: "Diamond Finishes API",
+      handler: sendEmailHandler,
+      cloudWatchRole: true
+    });
+  }
+}
 
 const app = new cdk.App();
-new DiamondfinishesBackendStack(app, 'DiamondfinishesBackendStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new DiamondfinishesBackendStack(app, 'DiamondfinishesBackendStack');
