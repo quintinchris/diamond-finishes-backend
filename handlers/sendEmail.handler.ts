bb934aa71a6eb3ba2bb4f09ebffdd424c0a3a30c
QuintinChris <quintinchrisdev@gmail.com>
2022-03-12 23:44:02 -0500

    adding initial handler

---
+++ handlers/sendEmail.handler.ts
@@ -0,0 +1,32 @@
+import {
+    Handler,
+    APIGatewayProxyEvent,
+    APIGatewayProxyResult,
+    Context,
+  } from "aws-lambda";
+  
+  export const sendEmailHandler: Handler<
+    APIGatewayProxyEvent,
+    APIGatewayProxyResult
+  > = async (event: APIGatewayProxyEvent) => {
+    let response: APIGatewayProxyResult;
+    try {
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "hello world",
+        }),
+      };
+    } catch (err) {
+      console.log(err);
+      response = {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: "some error happened",
+        }),
+      };
+    }
+  
+    return response;
+  };
+  
